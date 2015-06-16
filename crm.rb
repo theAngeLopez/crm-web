5262b414ca19addf9fe1d526acca2bbebe65b4d0
AngeLopez <angel@angelopez.ca>
2015-06-16 15:18:15 -0400

    crm and index updates

--- crm.rb
+++ crm.rb
@@ -2,23 +2,6 @@ require_relative 'contact'
 require_relative 'rolodex'
 require 'sinatra'
 
-@@rolodex = Rolodex.new
-
-get "/" do
-  @crm_app_name = "My CRM"
+get '/' do
   erb :index
-end
-
-get "/contacts" do
-  erb :contacts
-end
-
-get "/contacts/new" do
-  erb :new_contact
-end
-
-post "/contacts" do
-  new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
-  @@rolodex.add_contact(new_contact)
-  redirect to('/contacts')
 end
