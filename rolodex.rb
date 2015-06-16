14ec5fefb009a950febbf3b86ac03c012dace9e9
AngeLopez <angel@angelopez.ca>
2015-06-16 14:42:27 -0400

    The beginnings of my CRM web app

---
+++ rolodex.rb
@@ -0,0 +1,14 @@
+class Rolodex
+  attr_reader :contacts
+
+  def initialize
+    @contacts = []
+    @id = 1000
+  end
+
+  def add_contact(contact)
+    contact.id = @id
+    @contacts << contact
+    @id += 1
+  end
+end
