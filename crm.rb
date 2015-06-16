eedcf2b389ea16ee545641a545ddcb2d9e0f59c8
AngeLopez <angel@angelopez.ca>
2015-06-16 16:21:33 -0400

    New contact form created

--- crm.rb
+++ crm.rb
@@ -2,26 +2,28 @@ require_relative 'contact'
 require_relative 'rolodex'
 require 'sinatra'
 
+$rolodex= Rolodex.new
+
+# Routes
 get '/' do
   @crm_app_name = "Angel's CRM"
   erb :index
 end
 
 get '/contacts' do
-  @contacts = []
-  @contacts << Contact.new("Angel", "Lopez", "angel@lopez.com", "The awesomest")
-  @contacts << Contact.new("Bob", "Roberts", "bob@bob.com", "Robert squared")
-  @contacts << Contact.new("Nick", "Jonas", "the@cutest.com", "The cutest")
-  @contacts << Contact.new("Joe", "Fresh", "joe@fashion.com", "So fresh and clean")
-  @contacts << Contact.new("Michael", "Angelo", "blue@turtle.com", "Ninja master")
-  @contacts << Contact.new("Donna", "Tello", "orange@turtle.com", "Fave is pepperoni")
-  @contacts << Contact.new("Raph", "Phael", "red@turtle.com", "Likes pointy things")
-  @contacts << Contact.new("Leo", "Nardo", "purple@turtle.com", "Best friend is a rat")
-
   erb :contacts
 end
 
 get '/contacts/new' do
-  erb :add_contact
+  erb :new_contact
 end
 
+  # @contacts = []
+  # @contacts << Contact.new("Angel", "Lopez", "angel@lopez.com", "The awesomest")
+  # @contacts << Contact.new("Bob", "Roberts", "bob@bob.com", "Robert squared")
+  # @contacts << Contact.new("Nick", "Jonas", "the@cutest.com", "The cutest")
+  # @contacts << Contact.new("Joe", "Fresh", "joe@fashion.com", "So fresh and clean")
+  # @contacts << Contact.new("Michael", "Angelo", "blue@turtle.com", "Ninja master")
+  # @contacts << Contact.new("Donna", "Tello", "orange@turtle.com", "Fave is pepperoni")
+  # @contacts << Contact.new("Raph", "Phael", "red@turtle.com", "Likes pointy things")
+  # @contacts << Contact.new("Leo", "Nardo", "purple@turtle.com", "Best friend is a rat")
